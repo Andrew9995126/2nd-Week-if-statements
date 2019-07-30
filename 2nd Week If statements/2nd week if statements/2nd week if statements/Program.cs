0f486ece91e103f415a03ac6a6b63abe79c7f3cd
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-31 11:40:15 +1200

    commit 1

---
+++ 2nd Week If statements/2nd week if statements/2nd week if statements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_week_if_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 == num2)
+            { Console.WriteLine("These numbers are equal");
+            }
+
+            if (num1 > num2)
+            { Console.WriteLine($"The number {num1} is greater than {num2}");
+            }
+
+            if (num1<num2)
+            { Console.WriteLine($"The number {num1} is less than {num2}");
+            }
+
+            Console.ReadLine();
+             
+
+            
+        }
+    }
+}
